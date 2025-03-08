127c75a052434be02aed5316679069ad56492678
jhonnatan uruena <jhonnatan.uruena@sinco.com.co>
2025-03-07 20:17:34 -0500

    funcional enpoint

--- Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,16 +1,8 @@
-﻿
+﻿namespace Catalog.API.Products.CreateProduct;
 
-namespace Catalog.API.Products.CreateProduct;
-
-public record CreateProductRequest(string Name,
-    string Description,
-    List<string> Category,
-    string ImageFile,
-    decimal Price);
+public record CreateProductRequest(string Name, string Description, List<string> Category, string ImageFile, decimal Price);
 
 public record CreateProductResponse(Guid Id);
-
-
 
 public class CreateProductEndpoint : ICarterModule
 {
@@ -19,19 +11,17 @@ public class CreateProductEndpoint : ICarterModule
         app.MapPost("/products", async (CreateProductRequest request, ISender sender) =>
         {
             var command = request.Adapt<CreateProductCommand>();
-
             var result = await sender.Send(command);
-
             var response = request.Adapt<CreateProductResponse>();
-
             return Results.Ok(result.Id);
         })
-            .WithName("CreateProduct")
-            .Produces<CreateProductRequest>(StatusCodes.Status200OK)
-            .ProducesProblem(StatusCodes.Status400BadRequest)
-            .WithSummary("Create a new product")
-            .WithDescription("Create a new product in the catalog"); ;
+        .WithName("CreateProduct")
+        .Produces<CreateProductRequest>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Create a new product")
+        .WithDescription("Create a new product in the catalog");
 
 
+     
     }
 }
--- Services/Catalog/Catalog.API/Program.cs
+++ Services/Catalog/Catalog.API/Program.cs
@@ -1,6 +1,18 @@
+using Catalog.API.Products.CreateProduct;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCarter(configurator: c =>
+{
+    c.WithModule<CreateProductEndpoint>(); // Registrar módulos manualmente
+});
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapCarter();
+
 
 app.Run();
