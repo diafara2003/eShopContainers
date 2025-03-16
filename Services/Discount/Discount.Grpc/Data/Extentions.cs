e4529516d52376e273ed2adf22517b7fe3f67e66
jhonnatan uruena <jhonnatan.uruena@sinco.com.co>
2025-03-16 09:56:54 -0500

    migrations discount api

---
+++ Services/Discount/Discount.Grpc/Data/Extentions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data;
+
+public static class Extentions
+{
+    public static IApplicationBuilder UseMigration(this IApplicationBuilder app)
+    {
+        using var scope = app.ApplicationServices.CreateScope();
+        using var context = scope.ServiceProvider.GetRequiredService<DiscountContext>();
+        if (context.Database.GetPendingMigrations().Any())
+            context.Database.MigrateAsync();
+
+        return app;
+    }
+}
