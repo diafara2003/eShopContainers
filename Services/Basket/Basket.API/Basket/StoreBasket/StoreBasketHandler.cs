a0657da23e54c42ef55be45839be4f33374b8e10
jhonnatan uruena <jhonnatan.uruena@sinco.com.co>
2025-03-16 23:40:33 -0500

    calculte discount price

--- Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,11 +1,14 @@
-﻿namespace Basket.API.Basket.StoreBasket;
+﻿using System.Threading;
+using static Discount.Grpc.DiscountProtoService;
 
-public  record StoreBasketCommand(ShoppingCartRoot Cart):
+namespace Basket.API.Basket.StoreBasket;
+
+public record StoreBasketCommand(ShoppingCartRoot Cart) :
     ICommand<StoreBasketResult>;
 
 public record StoreBasketResult(string UserName);
 
-public class StoreBasketValidator:AbstractValidator<StoreBasketCommand>
+public class StoreBasketValidator : AbstractValidator<StoreBasketCommand>
 {
     public StoreBasketValidator()
     {
@@ -14,19 +17,35 @@ public class StoreBasketValidator:AbstractValidator<StoreBasketCommand>
     }
 }
 
-public class StoreBasketCommandHandler(IBasketRepository respository)
+public class StoreBasketCommandHandler(IBasketRepository respository, DiscountProtoServiceClient discount)
     : ICommandHandler<StoreBasketCommand, StoreBasketResult>
 {
     public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
     {
-        ShoppingCartRoot cart = command.Cart;
+        //communicate with discount Grpc and calculate lastest price of products in basket
 
-        var response =await respository.StoreBasket(cart,cancellationToken);
+        await DeductDiscount(command.Cart, cancellationToken);
+
+        //Store basket in database and update cache redis
+        var response = await respository.StoreBasket(command.Cart, cancellationToken);
 
         //store basket in database
 
         //update cache redis
 
         return new StoreBasketResult(command.Cart.UserName);
+    }
+
+    private async Task DeductDiscount(ShoppingCartRoot Cart, CancellationToken cancelationToken)
+    {
+        foreach (var item in Cart.Items)
+        {
+            var coupon = await discount.GetDiscountAsync(new Discount.Grpc.GetDiscountRequest
+            {
+                ProductName = item.ProductName
+            }, cancellationToken: cancelationToken);
+
+            item.Price -= coupon.Amount;
+        }
     }
 }
